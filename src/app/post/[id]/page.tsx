a1208f08d5a52edccac8d28bcd3de55b51d79aa3
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-09-18 15:16:17 +0100

    commit

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,18 +1,31 @@
-export async function generateStaticParams({ params }: any) {
-  const post = await fetch(
-    `https://mon-cours-next-js-13.vercel.app/api/posts/${params.id}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+// export async function generateMetadata({ params }: any) {
+//   const post: any = await fetch(`/api/posts/${params.id}`, {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
 
-  return {
-    title: post.title,
-  };
-}
+//   return {
+//     title: post.title,
+//     // description: post.title,
+//     // openGraph: {
+//     //   title: post.title,
+//     //   description: post.title,
+//     //   url: `/posts/${params.id}`,
+//     //   siteName: "Codewithguillaume",
+//     //   images: [
+//     //     {
+//     //       url: post.img_url,
+//     //       width: 1260,
+//     //       height: 800,
+//     //     },
+//     //   ],
+//     //   locale: "en-US",
+//     //   type: "website",
+//     // },
+//   };
+// }
 
 export default async function PostID({ params }: any) {
   return <div className="flex gap-2">Hello {params.id}</div>;
