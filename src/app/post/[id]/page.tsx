6df5446a2f164646af61b6009e446c068ebcd093
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2023-09-19 09:27:24 +0100

    commit

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,13 +1,33 @@
-// 1 . function fetchPost
+const BASE_URL = "https://mon-cours-next-js-13.vercel.app";
 
 const fetchPost = async (postId: string) => {
-  const post = await fetch(``, {
+  const post = await fetch(`${BASE_URL}/api/posts/${postId}`, {
     method: "GET",
   });
   return post.json();
 };
 
-// 2 . generateMetadata qui va appeller fetchpost
+export async function generateMetadata({ params }: any) {
+  const { post } = await fetchPost(params.id);
+
+  return {
+    title: post.title,
+    description: post.description,
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      url: `${BASE_URL}/post/${post.id}`,
+      type: "website",
+      siteName: post.title,
+      locale: "fr-FR",
+      images: [
+        {
+          url: `${BASE_URL}/${post.img_url}`,
+        },
+      ],
+    },
+  };
+}
 
 export default async function PostID({ params }: any) {
   return <div>hello {params.id}</div>;
